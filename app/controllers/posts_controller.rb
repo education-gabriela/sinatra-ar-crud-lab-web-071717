6a2c7715a9a94c73c29f32ae267319b38b915e25
gabidavila <gabidavila@gmail.com>
2017-08-09 15:41:17 -0400

    Done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+  get "/posts/new" do
+    erb :"posts/new"
+  end
+
+  post "/posts" do
+    Post.create(params)
+    redirect "/posts"
+  end
+
+  get "/posts" do
+    @all_posts = Post.all
+    erb :"posts/index"
+  end
+
+  get "/posts/:id/edit" do
+    @post = Post.find(params[:id])
+    erb :"posts/edit"
+  end
+
+  get "/posts/:id" do
+    @post = Post.find(params[:id])
+    erb :"posts/show"
+  end
+
+  patch "/posts/:id" do
+    @post = Post.find(params[:id])
+    # @post.name = params[:name]
+    # @post.content = params[:content]
+    # binding.pry
+    @post.update(name: params[:name], content: params[:content])
+
+    redirect "/posts/#{@post.id}"
+  end
+
+  delete "/posts/:id" do
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    erb :"/posts/delete"
+  end
+end
